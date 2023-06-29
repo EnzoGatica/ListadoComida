714ab1460d557f7e47b14e85b0e757be2ecec6d2
Enzo_Gatica <enzogatica96@gmail.com>
2023-06-28 22:57:16 -0400

    Segunda ventana, imagen funciona Check Point

--- app/src/main/java/com/example/listadocomida/SecondActivity.java
+++ app/src/main/java/com/example/listadocomida/SecondActivity.java
@@ -3,12 +3,56 @@ package com.example.listadocomida;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+
+import com.example.listadocomida.databinding.ActivityMainBinding;
+import com.example.listadocomida.databinding.ActivitySecondBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private ActivitySecondBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        ActivitySecondBinding binding = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        List<String> listaMenu = new ArrayList<>();
+
+        listaMenu.add("Puré de calabacín");
+        listaMenu.add("Tortilla de patatas");
+        listaMenu.add("Albóndigas de pollo");
+        listaMenu.add("Ensalada de tomate");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Lentejas vegetales");
+        listaMenu.add("Croquetas de atún");
+        listaMenu.add("Menestra de verduras");
+        listaMenu.add("Sardinas al horno");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Macarrones con verduras");
+        listaMenu.add("Pollo a la cerveza");
+        listaMenu.add("Sopa juliana");
+        listaMenu.add("Corvina al ajillo");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Leche, pan con aceite y fruta");
+        listaMenu.add("Leche, pan con aceite y fruta");
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listaMenu);
+        binding.ListaView.setAdapter(adapter);
+
     }
 }
